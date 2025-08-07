6a7f84da3bae73dd646068d6d23dff63ba402dc1
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-08 00:12:18 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+// Enable server-side rendering (SSR)
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'server' // This enables SSR for dynamic routes
 });
